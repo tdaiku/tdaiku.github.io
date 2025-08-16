b5ab4804c9f0072706b051e2c3ab3527d3847eca
大工智博 <t.daiku@wiz-net.jp>
2025-08-16 17:36:53 +0900

    info レイアウト調整

--- source/www/resources/js/top.js
+++ source/www/resources/js/top.js
@@ -8,6 +8,8 @@ class Top extends Common {
     initEvent() {
         super.initEvent();
         this.initModal();
+        this.initInformationNewMarks();
+        this.initModalContent();
     }
 
     initModal() {
@@ -20,14 +22,62 @@ class Top extends Common {
             awaitOpenAnimation: false,
             awaitCloseAnimation: false,
             onShow: (modal) => {
-                // eslint-disable-next-line no-console
-                console.log('Modal opened:', modal.id);
+                // モーダルが開いた時の処理
+                if (modal && modal.id) {
+                    // 必要に応じて処理を追加
+                }
             },
             onClose: (modal) => {
-                // eslint-disable-next-line no-console
-                console.log('Modal closed:', modal.id);
+                // モーダルが閉じた時の処理
+                if (modal && modal.id) {
+                    // 必要に応じて処理を追加
+                }
             },
         });
+    }
+
+    // InformationのNEWマークを自動設定
+    initInformationNewMarks() {
+        const informationLinks = document.querySelectorAll('.information-list a');
+        const currentDate = new Date();
+        const currentYear = currentDate.getFullYear();
+        const currentMonth = currentDate.getMonth() + 1; // 0ベースなので+1
+
+        informationLinks.forEach((link) => {
+            const dateElement = link.querySelector('.date');
+            if (dateElement) {
+                const dateText = dateElement.textContent.trim();
+                const dateMatch = dateText.match(/(\d{4})\.(\d{2})/);
+
+                if (dateMatch) {
+                    const linkYear = parseInt(dateMatch[1], 10);
+                    const linkMonth = parseInt(dateMatch[2], 10);
+
+                    // 当月以降の場合はnewクラスを追加
+                    if (linkYear > currentYear || (linkYear === currentYear && linkMonth >= currentMonth)) {
+                        link.classList.add('new');
+                    }
+                }
+            }
+        });
+    }
+
+    // モーダルのコンテンツを初期化
+    initModalContent() {
+        const informationList = document.querySelector('.information-list');
+        const modalList = document.querySelector('.modal-list');
+
+        if (informationList && modalList) {
+            // information-listの内容をクローンしてモーダルにコピー
+            const clonedContent = informationList.cloneNode(true);
+
+            // モーダル用のクラスに変更
+            clonedContent.classList.remove('information-list');
+            clonedContent.classList.add('modal-list');
+
+            // 既存のコンテンツを置き換え
+            modalList.innerHTML = clonedContent.innerHTML;
+        }
     }
 
     // eslint-disable-next-line class-methods-use-this
